0aefa188c61307d41985bafc12a0f6908134b059
panchovarela <fv6627@gmail.com>
2023-03-03 13:45:05 -0300

    Entrega de todas las tareas hasta la clase 7

---
+++ Primera Entrega/Clase 2- La Terminal/Alumnos/Francisco Varela/repaso_clase1y2/interfaces_de_usuario/repaso.cpp
@@ -0,0 +1,1 @@
+Una interfaz de usuario es el medio por el cual una persona interactúa con un software, dispositivo o sistema informático.
---
+++ Primera Entrega/Clase 2- La Terminal/Alumnos/Francisco Varela/repaso_clase1y2/software_y_hardware/repaso.js
@@ -0,0 +1,1 @@
+El hardware se refiere a los componentes físicos y tangibles de una computadora mientras que el software hace referencia a los programas, aplicaciones y sistemas operativos que se ejecutan en el hardware de un sistema informático.
